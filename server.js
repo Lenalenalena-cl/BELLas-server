df0dfd7f3c8dc0430773a7af569d6b953fc90f14
yvonneitan <itan3040@mylaurier.ca>
2025-02-16 12:33:44 -0700

    Update the backend port

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
+// import express from "express";
+// import cors from "cors";
+// import "dotenv/config";
+// import suggestionsRoutes from "./routes/suggestions-routes.js";
+// import votesRoutes from "./routes/votes-routes.js";
+
+// const app = express();
+
+// const PORT = process.env.PORT || 8080;
+// const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:";
+
+// app.use(cors());
+// app.use(express.static("public"));
+// app.use(express.json());
+
+// app.use("/suggestions", suggestionsRoutes);
+// app.use("/votes", votesRoutes);
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on ${BACKEND_URL}${PORT}`);
+// });
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
@@ -7,7 +28,7 @@ import votesRoutes from "./routes/votes-routes.js";
 const app = express();
 
 const PORT = process.env.PORT || 8080;
-const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:";
+const BACKEND_URL = process.env.BACKEND_URL || `http://localhost:${PORT}`;
 
 app.use(cors());
 app.use(express.static("public"));
@@ -17,5 +38,5 @@ app.use("/suggestions", suggestionsRoutes);
 app.use("/votes", votesRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${BACKEND_URL}${PORT}`);
+  console.log(`Server running on ${BACKEND_URL}`);
 });
